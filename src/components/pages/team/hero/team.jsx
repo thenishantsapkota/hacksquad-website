5b8a65296eb8b435bc6a26b148b256893ede3585
Nishant Sapkota <snishant306@gmail.com>
2022-10-04 19:06:58 +0545

    feat: add merged PRs on my-team page(idk if this works)

--- src/components/pages/team/hero/team.jsx
+++ src/components/pages/team/hero/team.jsx
@@ -149,6 +149,39 @@ const Team = ({ info }) => {
           ))}
         </div>
       </div>
+      <h2 className="mt-20 font-mono text-lg font-bold uppercase leading-tight lg:text-[25px] md:text-[25px] xs:text-[25px]">
+        {'>>'}Latest Merged Pull Requests
+      </h2>
+
+      <div className="md:scrollbar-hidden mx-auto mt-20 max-w-[1220px] bg-black md:max-w-none md:overflow-x-auto">
+        <div className="mt-5 md:min-w-[1080px] md:px-7 sm:px-4">
+          <div className="grid grid-cols-[230px_485px_230px_1fr] gap-x-5 border-b border-gray-2 pb-4 lg:grid-cols-[130px_390px_1fr_1fr] md:grid-cols-[130px_485px_230px_1fr] sm:grid-cols-[70px_150px_230px_1fr]">
+            <span className="font-medium uppercase">Place</span>
+            <span className="font-medium uppercase">Name</span>
+            <span className="font-medium uppercase">Pull</span>
+          </div>
+          {JSON.parse(info.team.prs || '[]').map((pr, index) => (
+            <ul>
+              <li className="grid grid-cols-[230px_485px_230px_1fr] gap-x-5 border-b border-gray-2 py-4 lg:grid-cols-[130px_390px_1fr_1fr] md:grid-cols-[130px_485px_230px_1fr] sm:grid-cols-[70px_150px_230px_1fr]">
+                <span>{index + 1}</span>
+                <p className="truncate font-medium">
+                  {pr.status === 'DELETED' && (
+                    <span className="font-bold" style={{ color: 'red' }}>
+                      DELETED:{' '}
+                    </span>
+                  )}
+                  {pr.title}
+                </p>
+                <p className="font-medium">
+                  <a href={pr.url} target="_blank" rel="noreferrer">
+                    <GitHubIcon className="h-[30px]" />
+                  </a>
+                </p>
+              </li>
+            </ul>
+          ))}
+        </div>
+      </div>
     </>
   );
 };
